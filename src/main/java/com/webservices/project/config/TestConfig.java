e72c52d95b7d16e1e04c6987cb3caf0b70900eb2
Joao Pedro <joao.pedro.durante.da.costa@hp.com>
2020-03-11 09:16:13 -0300

    Payment, one to one association

--- src/main/java/com/webservices/project/config/TestConfig.java
+++ src/main/java/com/webservices/project/config/TestConfig.java
@@ -63,6 +63,10 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
+
         p1.getCategories().add(cat2);
         p2.getCategories().add(cat1);
         p2.getCategories().add(cat3);
