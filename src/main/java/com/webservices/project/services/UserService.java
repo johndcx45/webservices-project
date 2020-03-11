1b5cf2e7c5af042f78fbccc5e3636df2dcfd31c5
Joao Pedro <joao.pedro.durante.da.costa@hp.com>
2020-03-11 12:45:00 -0300

    User insert

--- src/main/java/com/webservices/project/services/UserService.java
+++ src/main/java/com/webservices/project/services/UserService.java
@@ -22,4 +22,9 @@ public class UserService {
        Optional<User> obj = repository.findById(id);
        return obj.get();
     }
+
+
+    public User insertUser(User user){
+        return repository.save(user);
+    }
 }
